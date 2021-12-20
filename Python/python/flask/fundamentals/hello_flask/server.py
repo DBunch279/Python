77867335890f6b93917e87ead7f96bda6666811f
Dillon Bunch <dillon.bunch2765@gmail.com>
2021-12-19 21:41:42 -0500

    Python Hello Flask

---
+++ Python/python/flask/fundamentals/hello_flask/server.py
@@ -0,0 +1,19 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/cash')
+def cash():
+    return '$$$CASH$$$'
+
+@app.route('/users/<username>/<id>')
+def show_user_profile(username, id):
+    print(username)
+    print(id)
+    return "username: " + username + ", id: " + id
+
+if __name__=="__main__":
+    app.run(debug=True)
